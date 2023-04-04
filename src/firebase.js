1bd461997dfe89180f788268f2dedef4cd4c491f
Md Anzar Ahmad <anzarhps@gmail.com>
2023-04-04 15:10:26 +0530

    add homescreen

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBcS01QYS6Pyfw7yvFZ-FzlJE9oDS_ggGM',
+  authDomain: 'netflix-clone-625e6.firebaseapp.com',
+  projectId: 'netflix-clone-625e6',
+  storageBucket: 'netflix-clone-625e6.appspot.com',
+  messagingSenderId: '165806637168',
+  appId: '1:165806637168:web:99f87ff990e730ee01f4b5',
+  measurementId: 'G-NEEXVKJP77',
+}
